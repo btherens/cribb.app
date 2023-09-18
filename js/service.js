6a60adbebcdff084223a93b9c602f7590b897dd4
brian <brian.therens@gmail.com>
2023-09-18 17:48:19 -0500

    minor cleanup in service worker

--- js/service.js
+++ js/service.js
@@ -62,25 +62,6 @@ const routeFetch = event =>
     event.respondWith( response().then( response => response || fetch( event.request ) ) )
 }
 
-/* stale-while-revalidate (not used) */
-const staleWhileRevalidate = ( event, cachename ) => event.respondWith(
-    caches.match( event.request )
-        .then( ( cacheResponse ) =>
-        {
-            const networkResponse = fetch( event.request )
-                .then( ( networkResponse ) =>                {
-                    return caches.open( cachename )
-                        .then( ( cache ) =>
-                        {
-                            cache.put( event.request, networkResponse.clone() );
-                            return networkResponse;
-                        } )
-                }
-            );
-            return cacheResponse || networkResponse;
-        } )
-);
-
 /* generates a new response with argument as JSON object */
 const jsonResponse = ( o ) => new Response( JSON.stringify( o ), { headers: { 'Content-Type' : 'application/json' } } );
 
