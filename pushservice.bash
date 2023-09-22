3a03391b973b6920e8cffb23a98fef912b69b358
brian <brian.therens@gmail.com>
2023-09-22 17:46:06 -0500

    pushservice append errors to log

--- pushservice.bash
+++ pushservice.bash
@@ -11,7 +11,7 @@ function runservice() {
     if mkdir "$SERVICE.lock";
     then
         # run push service until we reach minute 59
-        while [ $(date "+%M") -lt 59 ]; do php "$SERVICE.php" 2>"$SERVICE.err"; done;
+        while [ $(date "+%M") -lt 59 ]; do php "$SERVICE.php" 2>>"$SERVICE.err"; done;
         # remove lock after service completes
         if ! rmdir "$SERVICE.lock"; then >&2 echo "failed to release lock"; fi;
     fi;
