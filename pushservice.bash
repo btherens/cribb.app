5b42fce5f6db7a648cca41da26df43c050e35508
brian <brian.therens@gmail.com>
2023-10-17 17:25:55 -0500

    refactor push service into functions and remove lock after timeout

--- pushservice.bash
+++ pushservice.bash
@@ -1,20 +1,45 @@
 #!/usr/bin/env bash
+usage() { cat << EOF
+usage: pushservice.bash [ -t TIMEOUT] [ -help]
 
-# run push service for one hour
-function runservice() {
-    # determine paths
-    THISDIR="$(dirname "$(readlink -f "$0")")";
-    SERVICE="$THISDIR/pushservice";
-    # delete lock if older than 90 minutes
-    # find "$SERVICE.lock" -type d -mmin +90 -exec rm -rdf {} \; >/dev/null 2>&1;
+run push service until timeout in seconds
+
+OPTIONS:
+    -t TIMEOUT    the timeout in seconds. default is 3540 (59 minutes)
+
+EOF
+exit 1; }
+while getopts :t: option
+do
+    case "${option}"
+    in
+        t) TIMEOUT=${OPTARG};;
+        ?) usage;;
+    esac
+done
+# define defaults
+[ -z "$TIMEOUT" ] && TIMEOUT=3540;
+# reset timer
+SECONDS=0;
+# determine paths
+THISDIR="$(dirname "$(readlink -f "$0")")";
+SERVICE="$THISDIR/pushservice";
+
+# open thread
+function openThread() {
+    # delete lock if older than timeout
+    find "$SERVICE.lock" -type d -mmin +$(( TIMEOUT / 60 )) -exec rm -rdf {} \; >/dev/null 2>&1;
     # lock folder
-    if mkdir "$SERVICE.lock";
-    then
-        # run push service until we reach minute 59
-        while [ $(date "+%M") -lt 59 ]; do php "$SERVICE.php" 2>>"$SERVICE.err"; done;
-        # remove lock after service completes
-        if ! rmdir "$SERVICE.lock"; then >&2 echo "failed to release lock"; fi;
-    fi;
+    mkdir "$SERVICE.lock";
+    # return exit code
+    return $?;
 }
-# call function
-runservice;
+
+# close thread
+function closeThread() { if ! rmdir "$SERVICE.lock"; then >&2 echo "failed to release lock"; fi; }
+
+# run service in loop until timeout
+function runservice() { while [ $SECONDS -lt $TIMEOUT ]; do php "$SERVICE.php" 2>>"$SERVICE.err"; done; }
+
+# run service
+if openThread; then runService; closeThread; fi;
